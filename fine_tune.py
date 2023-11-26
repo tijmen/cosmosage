cb5daea31c6e9ba8eca6a94851d83db67afc0531
Tijmen de Haan <tijmen.dehaan@gmail.com>
2023-11-26 17:10:45 +0900

    Speed up tokenization with multiprocessing library. Also cache the tokenized result.

--- fine_tune.py
+++ fine_tune.py
@@ -3,44 +3,99 @@ import time
 import logging
 import pickle
 import os
+import multiprocessing
+from random import shuffle
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
+def tokenize_chunk(chunk_data):
+    # helper function for tokenizing a chunk of text
+    # has to be outside of the class for multiprocessing (must be picklable)
+    chunk, tokenizer, max_length = chunk_data
+    tokens = tokenizer.encode(chunk, add_special_tokens=False)
+    return [tokens[i : i + max_length] for i in range(0, len(tokens), max_length)]
+
+
 class TextDataset(Dataset):
-    def __init__(self, json_file, tokenizer, max_length=512):
+    """
+    Dataset class for tokenizing a large text file.
+    """
+
+    def __init__(
+        self,
+        json_file,
+        tokenizer,
+        max_length=512,
+        cache_path=None,
+        num_cpus=None,
+        logger=None,
+    ):
         self.tokenizer = tokenizer
         self.max_length = max_length
         self.data = []
 
-        with open(json_file, "r", encoding="utf8") as file:
-            raw_data = json.load(file)
+        if cache_path is None:
+            raise ValueError("cache_path must be specified.")
+
+        if os.path.exists(cache_path):
+            logger.info("cached tokenization found, loading dataset")
+            with open(cache_path, "rb") as f:
+                self.data = pickle.load(f)
+        else:
+            logger.info("no cached tokenization found, tokenizing dataset now")
+            with open(json_file, "r", encoding="utf8") as file:
+                raw_data = json.load(file)
+            chunked_data = []
             for text in raw_data:
-                # Tokenize the text and check its length
-                tokens = tokenizer.encode(text, add_special_tokens=True)
-                if len(tokens) > max_length:
-                    # Split the text into chunks
-                    for i in range(0, len(tokens), max_length):
-                        chunk = tokens[i : i + max_length]
-                        self.data.append(tokenizer.decode(chunk))
-                else:
-                    self.data.append(text)
+                chunk_length = 50000
+                chunked_data.extend(
+                    [
+                        text[i : i + chunk_length]
+                        for i in range(0, len(text), chunk_length)
+                    ]
+                )
+            del raw_data  # release the memory
+
+            if num_cpus is None:
+                num_cpus = multiprocessing.cpu_count()
+
+            # Prepare data for multiprocessing
+            chunk_data = [(chunk, tokenizer, max_length) for chunk in chunked_data]
+            del chunked_data  # release the memory
+
+            # Tokenize the data in parallel
+            with multiprocessing.Pool(num_cpus) as pool:
+                tokenized_chunks = pool.map(tokenize_chunk, chunk_data)
+            del chunk_data  # release the memory
+
+            # Flatten and extend self.data
+            for chunks in tokenized_chunks:
+                self.data.extend(chunks)
+            del tokenized_chunks # release the memory
+
+            # Shuffle the chunks
+            shuffle(self.data)
+
+            logger.info("Saving tokenized data to cache")
+            with open(cache_path, "wb") as f:
+                pickle.dump(self.data, f)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        text = self.data[idx]
-        return self.tokenizer.encode_plus(
-            text,
-            add_special_tokens=True,
+        token_ids = self.data[idx]
+        encoded_dict = self.tokenizer.prepare_for_model(
+            token_ids,
             max_length=self.max_length,
             padding="max_length",
             truncation=True,
             return_attention_mask=True,
             return_tensors="pt",
         )
+        return encoded_dict
 
 
 def fine_tune(
@@ -51,14 +106,23 @@ def fine_tune(
     num_epochs=3,
     out_dir=None,
 ):
+    """
+    Fine-tune a pretrained model on a dataset using pytorch.
+    """
+
+    logging.basicConfig(
+        filename=os.path.join(out_dir, "fine_tune.log"),
+        filemode="w",
+        encoding="utf-8",
+        level=logging.INFO,
+    )
+
+    logging.info("start of log")
+
     if out_dir is None:
         raise ValueError("out_dir must be specified.")
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-
-    logging.basicConfig(
-        filename=os.path.join(out_dir, "fine_tune.log"), encoding="utf-8", level=logging.INFO
-    )
 
     # save arguments to log
     logging.info(f"pretrained_model_file_path: {pretrained_model_file_path}")
@@ -68,18 +132,25 @@ def fine_tune(
     logging.info(f"out_dir: {out_dir}")
     logging.info(f"num_epochs: {num_epochs}")
 
-    logging.info("Starting fine-tuning.")
-
+    logging.info("initialize tokenizer")
     tokenizer = AutoTokenizer.from_pretrained(pretrained_model_file_path)
-    dataset = TextDataset(training_data, tokenizer)
-    dataloader = DataLoader(dataset, batch_size=3, shuffle=True)
+    logging.info("tokenize dataset")
+    cache_path = os.path.join(out_dir, "tokenized_dataset.pkl")
+    dataset = TextDataset(
+        training_data, tokenizer, cache_path=cache_path, logger=logging.getLogger()
+    )
+
+    logging.info("initialize dataloader")
+    dataloader = DataLoader(dataset, batch_size=3)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f"device: {device}")
+    logging.info("initialize model")
     model = (
         AutoModelForCausalLM.from_pretrained(pretrained_model_file_path)
         .to(device)
         .to(dtype=torch.bfloat16)
     )
-
+    logging.info("initialize optimizer")
     # optimizer = AdamW(model.parameters(), lr=5e-5)
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
     start_time = time.time()
@@ -87,6 +158,7 @@ def fine_tune(
     save_id = 0
     loss_history = []
     model.train()
+    logging.info("start training loop")
     for epoch in range(num_epochs):
         total_loss = 0
         for batch_idx, batch in enumerate(dataloader):
@@ -130,7 +202,7 @@ def fine_tune(
                 save_id += 1
                 model.save_pretrained(f"{out_dir}/cp_{save_id}")
                 last_save_time = time.time()
-            
+
         avg_loss = total_loss / len(dataloader)
         print(f"Epoch {epoch + 1}/{num_epochs}, Average Loss: {avg_loss}")
 
